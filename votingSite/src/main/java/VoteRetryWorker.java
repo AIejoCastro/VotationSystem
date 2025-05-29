bee26f2281d1c89b88f312fb193b02bdb5852e68
AIejoCastro <alejocastro20@hotmail.com>
2025-05-29 18:31:22 -0500

    Time to resend in config and rename directories

--- votingSite/src/main/java/VoteRetryWorker.java
+++ votingSite/src/main/java/VoteRetryWorker.java
@@ -2,15 +2,19 @@ import Demo.*;
 import com.zeroc.Ice.LocalException;
 import com.zeroc.IceGrid.QueryPrx;
 import com.zeroc.Ice.Communicator;
+import com.zeroc.Ice.Properties;
 
 import java.util.List;
 
 public class VoteRetryWorker extends Thread {
     private final Communicator communicator;
     private final OfflineVoteQueue queue = new OfflineVoteQueue();
+    private final long retryInterval;
 
     public VoteRetryWorker(Communicator communicator) {
         this.communicator = communicator;
+        Properties props = communicator.getProperties();
+        this.retryInterval = props.getPropertyAsIntWithDefault("VoteRetry.Interval", 10000); // en milisegundos
     }
 
     @Override
@@ -21,12 +25,14 @@ public class VoteRetryWorker extends Thread {
                 if (!votes.isEmpty()) {
                     VotationPrx hello = getActiveProxy();
                     if (hello == null) {
+                        System.out.println("No hay servidor disponible para reintentar los votos pendientes.");
                     } else {
                         boolean allSuccess = true;
                         for (String[] vote : votes) {
                             try {
                                 hello.sendVote(vote[0], vote[1]);
                             } catch (LocalException | AlreadyVotedException e) {
+                                System.out.println("Error reintentando voto: " + e.getMessage());
                                 allSuccess = false;
                             }
                         }
@@ -38,7 +44,7 @@ public class VoteRetryWorker extends Thread {
                     }
                 }
 
-                Thread.sleep(10000); // 10 segundos entre reintentos
+                Thread.sleep(retryInterval);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
